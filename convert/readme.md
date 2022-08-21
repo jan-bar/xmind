c9253764bbebb6a02f5942ebd2307c81ebf05965
janbar <janbar@qq.com>
2022-08-21 11:18:08 +0800

    增加有道云笔记转换

--- convert/readme.md
+++ convert/readme.md
@@ -1,3 +1,4 @@
 各种思维导图转换为`xmind`
 * 亿图的思维导图转换: [edrawmax](edrawmax/edrawmax.go)
+* 有道云笔记思维导图转换: [youdao](youdao/youdao.go)
 * 
---
+++ convert/youdao/youdao.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/jan-bar/xmind"
+)
+
+func main() {
+	if len(os.Args) != 3 {
+		return
+	}
+	err := YouDao(os.Args[1], os.Args[2])
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func YouDao(src, dst string) error {
+	data, err := os.ReadFile(src)
+	if err != nil {
+		return err
+	}
+
+	var node struct {
+		Nodes json.RawMessage `json:"nodes"`
+	}
+	err = json.Unmarshal(data, &node)
+	if err != nil {
+		return err
+	}
+
+	// 有道云笔记思维导图,符合数组形式的结构,用自定义类型直接就可以转换
+	st, err := xmind.LoadCustom([]byte(node.Nodes), "id", "topic", "parentid")
+	if err != nil {
+		return err
+	}
+
+	return xmind.SaveSheets(dst, st)
+}
